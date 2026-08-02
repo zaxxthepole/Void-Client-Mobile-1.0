ed6a485226dd948e5f80d388266e5742cc6144cc
VoidClient Developer <voidclient@example.com>
2026-08-02 16:35:13 +0800

    chore: rename app to Voidclient (drop '1.0'), bump to v1.2 (versionCode 2)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         minSdk = 28
         //noinspection OldTargetApi,EditedTargetSdkVersion
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
